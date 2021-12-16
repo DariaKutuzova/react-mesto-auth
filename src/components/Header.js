d257c9d16cf95b61ed37ae9054a8d280c3ec1876
DariaKutuzova <kutuzovad94@gmail.com>
2021-12-17 00:51:06 +0300

    feat: Мобильная версия+меню

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,54 @@
 import logo from "../images/logo.svg";
 import {useLocation, Link} from "react-router-dom";
+import {useState} from "react";
 
 function Header({loggedIn, onSingOut, autoEmail}) {
 
+    const [isActive, setIsActive] = useState(false);
+
     const address = useLocation()
+
+    const menu = (
+        <nav className={`header__entry ${isActive ? 'header__show-menu' : ''}`}>
+            <p className="header__text">{autoEmail}</p>
+            <button className="header__button" type="button" onClick={handleSignOut}>Выйти</button>
+        </nav>
+    )
 
     function handleSignOut() {
         onSingOut();
     }
 
+    function handleNavi() {
+        setIsActive(!isActive);
+
+    }
+
     return (
         <header className="header page__item">
-            <img src={logo} alt="Лого шапки" className="header__logo"/>
-            {loggedIn && (
-                <p className="header__entry">{autoEmail}
+            <div className={`header__menu ${!isActive ? 'header__menu_active' : ''}`}>
+                <nav className={`header__entry header__show-menu`}>
+                    <p className="header__text">{autoEmail}</p>
                     <button className="header__button" type="button" onClick={handleSignOut}>Выйти</button>
-                </p>
-            )}
+                </nav>
+            </div>
+            <div className="header__basic">
+                <img src={logo} alt="Лого шапки" className="header__logo"/>
+                {loggedIn && (
+                    <>
+                            <nav className="header__entry">
+                                <p className="header__text">{autoEmail}</p>
+                                <button className="header__button" type="button" onClick={handleSignOut}>Выйти</button>
+                            </nav>
+                        <button className="header__navi" type="button" onClick={handleNavi}>
+                            <span
+                                className={`header__navi-line 
+                                ${isActive ? 'header__navi-line_active' : ''}`}>
+                            </span>
+                        </button>
+                    </>
+                )}
+            </div>
             {!loggedIn && (
                 <p>
                     {address.pathname === '/sign-in' &&
